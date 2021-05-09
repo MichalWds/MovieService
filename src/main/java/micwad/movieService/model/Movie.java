2db912bbdb4a767c43f99f3dacad8a4be756d380
Wadas <mawd@gft.com>
2021-05-09 16:54:25 +0200

    3a

---
+++ src/main/java/micwad/movieService/model/Movie.java
@@ -0,0 +1,41 @@
+package micwad.movieService.model;
+
+public class Movie {
+
+    long id;
+    String name;
+    String category;
+
+    public Movie() {
+    }
+
+    public Movie(long id, String name, String category) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
